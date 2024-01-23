7ab6b6da6dd0851684c8f150fdc44564958c9158
Navyavaidy <navyavaidyanathan1999@gmail.com>
2024-01-23 13:43:42 +0530

    Service center trial 1
    This has one animation

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1932389.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 162912288, 162912289, 162912290, 162912291, 162976160, 163347056, 163461208, 163484716, 163486699, 163970530, 164120742, 164121139, 164121477, 164122548, 164122900, 164128271, 164207740, 164456370, 164472073 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/162912312/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/162912313/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/162912311/1/ammo.js', 'preload' : true},
+];
